948973857f7ccf46584d76fca1e574e49e3529ac
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2022-10-27 07:34:03 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -122,5 +122,12 @@ function updateCardNumber(number){
 }
 
 expirationDateMasked.on("accept", () => {
-  
+
 })
+
+function updateExpirationDate(date){
+  const ccExpiration = document.querySelector(".cc-extra .value")
+  ccExpiration.innerText = date.length === 0 ?
+  "02/32" : date 
+
+}
